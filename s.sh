e86be9178d2f9037dbf7960c8af9ef516e407812
Brian Gershon <brian.five@gmail.com>
2013-03-08 10:50:27 -0800

    Add a brief status about the repository you're in

--- s.sh
+++ s.sh
@@ -8,18 +8,21 @@
 clear
 
 if `git rev-parse 2> /dev/null`; then
+	echo "---\nGit Repository Status\n---\n"
 	git status
 	exit
 fi
 
 if `hg -q stat 2> /dev/null`; then
+	echo "---\nMercurial Repository Status\n("`hg id`")\n---\n"
 	hg status
 	exit
 fi
 
 if `svn info 1> /dev/null 2> /dev/null`; then
+	echo "---\nSubversion Repository Status\n("`svn info | grep '^URL' | awk '{print $NF}'`")\n---\n"
 	svn st
 	exit
 fi
 
-echo "No git, hg or svn repository found here."
+echo "---\nA git, mercurial or subversion repository not found.\n---\n"
